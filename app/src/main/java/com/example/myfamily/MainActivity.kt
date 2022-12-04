d131de35f1186b49ae4fb1a931de2a198c9fb8d6
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2022-12-04 14:06:16 +0500

    init project

---
+++ app/src/main/java/com/example/myfamily/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myfamily
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottombar=findViewById<BottomNavigationView>(R.id.bottom_bar)
+        bottombar.setOnItemSelectedListener {
+            if (it.itemId == R.id.nav_guard){
+                inflateFragment(GuardFragment.newInstance())
+            }else if(it.itemId == R.id.nav_home){
+                inflateFragment(HomeFragment.newInstance())
+            }else if(it.itemId == R.id.nav_dashboard){
+                inflateFragment(DashboardFragment.newInstance())
+            }else if(it.itemId == R.id.nav_profile){
+                inflateFragment(ProfileFragment.newInstance())
+            }
+        true
+        }
+    }
+
+    private fun inflateHomeFragment() {
+    }
+
+    private fun inflateFragment(newInstance: Fragment) {
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,newInstance)
+        transaction.commit()
+    }
+}
